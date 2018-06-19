6c3c8bdc85c394d181bfa8cb717fd10aef4d0d7d
陈小双 <chenxiaoshuang.qingtingfm.com>
2018-06-19 19:00:07 +0800

    下载缓冲池

--- 下载管理器/LDDownloaderManager.m
+++ 下载管理器/LDDownloaderManager.m
@@ -8,8 +8,28 @@
 
 #import "LDDownloaderManager.h"
 #import "LDDownloader.h"
+@interface LDDownloaderManager()
+/** 下载缓冲池 */
+@property (nonatomic, strong) NSMutableDictionary *downloaderCache;
+
+
+
+@end
 
 @implementation LDDownloaderManager
+
+- (NSMutableDictionary *)downloaderCache {
+    if (!_downloaderCache) {
+        _downloaderCache = [NSMutableDictionary dictionary];
+    }
+    return _downloaderCache;
+}
+
+/**
+每次实例化一个 HKDownloader 对应一个文件的下载操作!!
+如果才操作没有执行完毕,不需要再次开启!
+解决思路: 下载缓冲池!
+*/
 +(instancetype)sharedDownloaderManager {
     static LDDownloaderManager *manager;
     static dispatch_once_t onceToken;
@@ -20,8 +40,13 @@
 }
 
 -(void)downloadWithURL:(NSURL *)url progress:(void (^)(float progress))progress completion:(void (^)(NSString * filePath))completion failed:(void (^)(NSString * errorMsg))failed {
+    if ([self.downloaderCache objectForKey:url.path]) {
+        NSLog(@"下载任务已存在");
+        return;
+    }
     LDDownloader *downloader = [[LDDownloader alloc] init];
     [downloader downloadWithURL:url progress:progress completion:failed failed:failed];
+    [self.downloaderCache setObject:downloader forKey:url.path];
 }
 
 @end
